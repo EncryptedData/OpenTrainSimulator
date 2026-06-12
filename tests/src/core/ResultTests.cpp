44ba44b8431628e82a0ddcce2c1b97b3d2fdc163
Russell Johnson <russell.johnson@superdark.net>
2026-06-11 19:13:41 -0700

    Added a Result class and integrated a Unit testing framework (#1)
    * Added a Result class and integrated a Unit testing framework
    
    * Added unit test to check for operator bool for result

---
+++ tests/src/core/ResultTests.cpp
@@ -0,0 +1,38 @@
+#include <catch2/catch_all.hpp>
+
+#include <Result.hpp>
+
+TEST_CASE("Result constructed without value should return no value")
+{
+    OpenTrainSimulator::Result<int> result;
+
+    REQUIRE_FALSE(result.has_value());
+}
+
+TEST_CASE("Result constructed with value should return the value")
+{
+    OpenTrainSimulator::Result<int> result(0);
+
+    REQUIRE(result.has_value());
+    REQUIRE(result.value() == 0);
+}
+
+TEST_CASE("Result can be used in if statement")
+{
+    OpenTrainSimulator::Result<int> result(0);
+
+    REQUIRE(result);
+    REQUIRE(!result == false);
+}
+
+TEST_CASE("Result can be constructed with non-builtin types")
+{
+    struct Data
+    {
+    };
+
+    OpenTrainSimulator::Result<Data> result(Data{});
+
+    REQUIRE(result.has_value());
+}
+
