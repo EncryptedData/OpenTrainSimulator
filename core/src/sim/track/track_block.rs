e9abd2807690274ca86d2ce2b2d7d6d4c3d456af
Russell J <russell.johnson@superdark.net>
2026-09-19 11:12:35 -0700

    Added track block struct

--- core/src/sim/track/track_block.rs
+++ core/src/sim/track/track_block.rs
@@ -1,7 +1,54 @@
-use crate::sim::track::primitive_types::TrackBlockStatus;
+use std::cmp::PartialEq;
 
-trait TrackBlock {
-    fn get_status(&self) -> TrackBlockStatus;
+#[derive(PartialEq)]
+pub enum TrackBlockHazardType {
+    Railcar,
+    DerailedRailcar,
+    EnvironmentalHazard,
+    Unknown,
+}
 
-    fn set_status(&mut self, status: TrackBlockStatus);
+/// What is the status of the signal block
+pub enum TrackBlockStatus {
+    /// The block has a hazard, or the state is unknown
+    Fault,
+
+    /// The block is currently occupied by something
+    Occupied,
+
+    /// Nothing is currently occupying the block and it's ready
+    Clear,
+}
+
+pub struct TrackBlock {
+    hazards: Vec<TrackBlockHazardType>,
+}
+
+impl TrackBlock {
+    pub fn new() -> Self {
+        Self {
+            hazards: Vec::new(),
+        }
+    }
+
+    pub fn get_status(&self) -> TrackBlockStatus {
+        if self.hazards.is_empty() {
+            TrackBlockStatus::Clear
+        } else {
+            if self.hazards.iter().any(|x: &TrackBlockHazardType| { *x != TrackBlockHazardType::Railcar }) {
+                TrackBlockStatus::Fault
+            }
+            else {
+                TrackBlockStatus::Occupied
+            }
+        }
+    }
+
+    pub fn set_hazard(&mut self, hazard_type: TrackBlockHazardType) {
+        self.hazards.push(hazard_type);
+    }
+
+    pub fn remove_hazard(&mut self, hazard_type: TrackBlockHazardType) {
+        self.hazards.retain(|h| h != &hazard_type);
+    }
 }
